943074b79223c4ee7a5c31856641e9d9e5a77655
Evgeniya <122976396+evvrod@users.noreply.github.com>
2024-08-22 15:30:51 +0300

    Revert "feat/RSS-REACT_46/create basic routing"
    This reverts commit fdf3cb61ddcf76f3d80eda34ec99f0d018c92abc.

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
-  plugins: ['react-compiler'],
-  rules: {
-    'no-console': 'error',
-    'react-compiler/react-compiler': 'error',
-  },
-};
