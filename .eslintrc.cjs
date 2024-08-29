df00b7db1c62a593ac921792305ccaba82432b94
Elena <elen.vasileva@gmail.com>
2024-08-29 18:45:28 +0200

    update linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,12 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
+  extends: [
+    'next/core-web-vitals',
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
   plugins: ['react-compiler'],
   rules: {
     'no-console': 'error',
