2108f6c95131d99f879f234a87774382da7a41e7
Elena <elen.vasileva@gmail.com>
2024-09-01 01:30:40 +0200

    move header upper

--- src/components/RestLayout/RestLayout.tsx
+++ src/components/RestLayout/RestLayout.tsx
@@ -1,13 +1,14 @@
-import Header from './Header/Header';
 import RestQueryComponent from './RestQueryComponent/RestQueryComponent';
 import styles from './RestLayout.module.scss'
+import PageHeader from 'components/PageHeader/PageHeader';
 
 const RestLayout = () => {
   return (
     <div className={styles.restLayout}>
-      <Header>REST client</Header>
+      <PageHeader>REST client</PageHeader>
       <RestQueryComponent />
     </div>
   );
 };
+
 export default RestLayout;
