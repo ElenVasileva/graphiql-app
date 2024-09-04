84023e776771810b1add34914f40f0aff0cb2c39
Elena <elen.vasileva@gmail.com>
2024-09-05 00:27:21 +0200

    finish the REST response component

--- src/components/RestLayout/RestResponseComponent/RestResponseComponent.tsx
+++ src/components/RestLayout/RestResponseComponent/RestResponseComponent.tsx
@@ -1,15 +1,26 @@
 'use client';
 
+import { useState } from 'react';
 import styles from './RestResponseComponent.module.scss';
 import { RestResponse } from 'types/RestResponse';
+import { Button } from 'components/Button';
+
+enum TabSection {
+  Pretty = 'Pretty',
+  Raw = 'Raw',
+}
+
+const sectionList: TabSection[] = [TabSection.Pretty, TabSection.Raw];
 
 const RestResponseComponent = ({
   response,
 }: {
   response: RestResponse | undefined;
 }) => {
+  const [section, setSection] = useState<TabSection>(TabSection.Pretty);
+
   const numberOfSpaces = 4;
-  const body =
+  const prettyBody =
     response && response.body
       ? JSON.stringify(JSON.parse(response.body), undefined, numberOfSpaces)
       : '';
@@ -18,12 +29,32 @@ const RestResponseComponent = ({
     <div className={styles.response}>
       {!!response && (
         <>
-          <div className={styles.response__status}>
-            Status: {response.status}
+          <div className={styles.response__statusAndSection}>
+            <div className={styles.response__status}>
+              Status: {response.status}
+            </div>
+            {!!response.body && (
+              <div className={styles.response__sectionSelector}>
+                {sectionList.map((sec) => (
+                  <Button
+                    key={sec}
+                    className={section === sec ? styles.active : ''}
+                    onClick={() => setSection(sec)}
+                  >
+                    {sec.toString()}
+                  </Button>
+                ))}
+              </div>
+            )}
           </div>
-          {!!body && (
-            <div>
-              <textarea readOnly value={body} />
+          {!!response.body && (
+            <div className={styles.response__textareaContainer}>
+              <textarea
+                readOnly
+                value={
+                  section === TabSection.Pretty ? prettyBody : response.body
+                }
+              />
             </div>
           )}
         </>
