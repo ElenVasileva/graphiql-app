59542ed591970b0a37efeb01d1d2a424715b0579
ElenVasileva <157320764+ElenVasileva@users.noreply.github.com>
2024-09-06 16:05:49 +0200

    Feat/rss react 13/input for url (#16)
    * add input for url
    * refactor rest query component

--- src/app/restful/[method]/page.tsx
+++ src/app/restful/[method]/page.tsx
@@ -1,15 +1,3 @@
-import ResponseComponent from 'components/ResponseComponent/ResponseComponent';
-import { HttpMethod } from 'constants/methodTypes';
-
-export default function Page({
-  params: { method },
-}: {
-  params: { method: string };
-}) {
-  const httpMethod = (method as HttpMethod) || HttpMethod.get;
-  return (
-    <>
-      <ResponseComponent method={httpMethod} />
-    </>
-  );
+export default function Page() {
+  return <></>;
 }
--- src/components/RestLayout/RestLayout.tsx
+++ src/components/RestLayout/RestLayout.tsx
@@ -1,12 +1,37 @@
 import RestQueryComponent from './RestQueryComponent/RestQueryComponent';
 import styles from './RestLayout.module.scss';
 import PageHeader from 'components/PageHeader/PageHeader';
+import RestResponseComponent from './RestResponseComponent/RestResponseComponent';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+import { RestResponse } from 'types/RestResponse';
+import { url2RestRequest } from 'utils/restUrlConverter';
 
 const RestLayout = () => {
+  const path = usePathname();
+  const [response, setResponse] = useState<RestResponse | undefined>(undefined);
+
+  const onSubmit = async () => {
+    const requestFromUrl = url2RestRequest(path, true);
+
+    const rawResponse = await fetch(`/api`, {
+      method: 'POST',
+      body: JSON.stringify(requestFromUrl),
+    });
+    const json = (await rawResponse.json()) as RestResponse;
+    setResponse(json);
+  };
   return (
     <div className={styles.restLayout}>
       <PageHeader>REST client</PageHeader>
-      <RestQueryComponent />
+      <div className={styles.restLayout__container}>
+        <div className={styles.restLayout__item}>
+          <RestQueryComponent onSubmit={onSubmit} />
+        </div>
+        <div className={styles.restLayout__item}>
+          <RestResponseComponent response={response} />
+        </div>
+      </div>
     </div>
   );
 };
