79627c7664caddd6fe16aaf0c54a03c55137d6ea
Elena <elen.vasileva@gmail.com>
2024-09-15 02:57:16 +0200

    revert import path

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,7 +1,7 @@
 import Greeting from 'components/MainPage/Greeting/Greeting';
 import styles from './MainPage.module.scss';
 import SignLinks from 'components/MainPage/SignLinks/SignLinks';
-import InformationBlock from '@/components/MainPage/InformationBlock/InformationBlock';
+import InformationBlock from 'components/MainPage/InformationBlock/InformationBlock';
 import ContentLinks from 'components/MainPage/ContentLinks/ContentLinks';
 
 const MainPage = ({ session }: { session: string | null }) => {
